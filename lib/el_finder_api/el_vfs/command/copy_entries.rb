cda87bf802936458463051806ffdb962f6d3db69
Dmitry Lihachev <lda@openteam.ru>
2011-11-17 16:10:23 +0700

    #8782. command: copy_entries (paste)

--- lib/el_finder_api/el_vfs/command/copy_entries.rb
+++ lib/el_finder_api/el_vfs/command/copy_entries.rb
@@ -1,3 +1,42 @@
-class ElVfs::Command::CopyEntries < ElVfs::Command
-  register_in_connector :paste
+module ElVfs
+  class Command::CopyEntries < ElVfs::Command
+    register_in_connector :paste
+
+    options :src, :dst, :targets, :cut
+
+
+    protected
+      def hash
+        if source && destination && entries.any? && entries.all?
+          {:added => copy_entries, :removed => removed_entries.map(&:target)}
+        else
+          wrong_params_hash
+        end
+      end
+
+    private
+      def removed_entries
+        cut ? entries.map(&:destroy) : []
+      end
+
+      def copy_entries
+        entries.map do | entry |
+          entry.dup.tap do | entry |
+            entry.update_attributes! :parent => destination
+          end
+        end
+      end
+
+      def source
+        @source ||= Entry.only_directories.find_by_entry_path_hash(src)
+      end
+
+      def destination
+        @destination ||= Entry.only_directories.find_by_entry_path_hash(dst)
+      end
+
+      def entries
+        @entries ||= [*targets].map{|hash| source.children.find_by_entry_path_hash(hash)}
+      end
+  end
 end
