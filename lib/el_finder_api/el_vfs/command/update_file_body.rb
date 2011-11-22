1a21988682658cef7e12b9fd2ca71127553889a6
Dmitry Lihachev <lda@openteam.ru>
2011-11-22 16:22:01 +0700

    #8834. command: update_file_body (edit)

--- lib/el_finder_api/el_vfs/command/update_file_body.rb
+++ lib/el_finder_api/el_vfs/command/update_file_body.rb
@@ -1,27 +1,27 @@
 module ElVfs
-  class ElVfs::Command::UpdateFileBody < ElVfs::Command
+  class Command::UpdateFileBody < Command
     register_in_connector :put
 
-    options :target, :content
+    class Arguments < Command::Arguments
+      attr_accessor :target, :content
+      validates_presence_of :target, :content
+      validates :entry, :is_a_file => true
+    end
+
+    class Result < Command::Result
+      def added
+        [execute_command]
+      end
+    end
 
     protected
 
-      def hash
-        if target && file && content
-          { :added => update_file_body }
-        else
-          wrong_params_hash
-        end
-      end
-
-    private
-
-      def update_file_body
+      def execute_command
         Dir.mktmpdir do |dir|
-          content_file =  ::File.open("#{dir}/#{file.entry_name}", "w"){|f|f.write(content)}
-          file.entry = ::File.open("#{dir}/#{file.entry_name}")
+          content_file =  ::File.open("#{dir}/#{arguments.entry.entry_name}", "w"){|f|f.write(arguments.content)}
+          arguments.entry.entry = ::File.open("#{dir}/#{arguments.entry.entry_name}")
         end
-        file
+        arguments.entry
       end
 
   end
--- spec/models/el_vfs/command/update_file_body_spec.rb
+++ spec/models/el_vfs/command/update_file_body_spec.rb
@@ -6,13 +6,13 @@ module ElVfs
 
   describe Command::UpdateFileBody do
     let(:params)        { {target: target, :content => 'new content ololo'} }
-    let(:command)       { Command::UpdateFileBody.new params }
+    let(:command)       { described_class.new params }
     let(:subject)       { command.result }
     let(:file)          { Fabricate(:file) }
     let(:target)        { file.target }
 
     describe 'target: file' do
-      its(:added)       { subject.entry_size.should == 17 }
+      its(:added)       { subject.map(&:entry_size).should == [17] }
     end
   end
 
